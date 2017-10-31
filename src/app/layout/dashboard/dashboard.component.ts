790624a1a9facfea985fe982e15ead272002305b
Naveen Prabhaker <nprabhaker@playags.com>
2017-10-30 17:35:08 -0700

    Cosmetic Changes

--- src/app/layout/dashboard/dashboard.component.ts
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -14,9 +14,9 @@ export class DashboardComponent implements OnInit {
     constructor() {
         this.sliders.push({
             imagePath: 'assets/images/slider1.jpg',
-            label: 'First slide label',
-            text: 'Nulla vitae elit libero, a pharetra augue mollis interdum.'
-        }, {
+            label: 'Community Initiative',
+            text: 'A community based self serving initiative, to help financial needs'
+        }, /*{
             imagePath: 'assets/images/slider2.jpg',
             label: 'Second slide label',
             text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
@@ -24,7 +24,8 @@ export class DashboardComponent implements OnInit {
             imagePath: 'assets/images/slider3.jpg',
             label: 'Third slide label',
             text: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.'
-        });
+        }*/
+    );
 
         this.alerts.push({
             id: 1,
